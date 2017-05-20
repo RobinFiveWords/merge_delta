b0f9640339daed989da557fcf0ab5f18c6b014c5
RobinFiveWords <robinfishbein@Robins-MacBook-Pro.local>
2017-05-20 15:44:16 -0500

    Added delta_robust which codes and decodes index_cols to avoid hash collision

--- delta.py
+++ delta.py
@@ -41,3 +41,28 @@ def delta(df1, df2, index_cols=None, suffixes=('_1', '_2'),
         print()
         print(full._merge.value_counts().rename(index=remappings))
     return full, add, change, delete
+
+
+def delta_robust(df1, df2, index_cols, **kwargs):
+    """Compute delta between two files while encoding index_cols
+    to prevent hash collision."""
+    df1 = df1.copy()
+    df2 = df2.copy()
+    if isinstance(index_cols, pd.Index):
+        index_cols = list(index_cols)
+    kwargs.pop('reset_indexes', None)
+    cats = [pd.Categorical(np.union1d(*[df[col].values for df in [df1, df2]]))
+              .categories
+            for col in index_cols]
+    types = [df1[col].dtype for col in index_cols]
+
+    for i, col in enumerate(index_cols):
+        df1.loc[:, col] = df1[col].astype('category', categories=cats[i]).cat.codes
+        df2.loc[:, col] = df2[col].astype('category', categories=cats[i]).cat.codes
+    f, a, c, d = delta(df1, df2, index_cols, **kwargs)
+    for i, col in enumerate(index_cols):
+        f[col] = pd.Categorical.from_codes(f[col], cats[i]).astype(types[i])
+        a[col] = pd.Categorical.from_codes(a[col], cats[i]).astype(types[i])
+        c[col] = pd.Categorical.from_codes(c[col], cats[i]).astype(types[i])
+        d[col] = pd.Categorical.from_codes(d[col], cats[i]).astype(types[i])
+    return f, a, c, d
