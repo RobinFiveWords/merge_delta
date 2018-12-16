534cb65f0d680bc48159de39ef5b7e8ce8036de4
RobinFiveWords <robinfivewords+github@gmail.com>
2018-12-16 14:45:30 -0600

    added delta_robust2 for pandas >= 0.21

--- delta.py
+++ delta.py
@@ -1,3 +1,9 @@
+import pandas as pd
+import numpy as np
+if pd.__version__ >= '0.21':
+    from pandas.api.types import CategoricalDtype
+
+
 def delta(df1, df2, index_cols=None, suffixes=('_1', '_2'),
           reset_indexes=True, value_counts=True):
     if isinstance(index_cols, pd.Index):
@@ -45,7 +51,9 @@ def delta(df1, df2, index_cols=None, suffixes=('_1', '_2'),
 
 def delta_robust(df1, df2, index_cols, **kwargs):
     """Compute delta between two files while encoding index_cols
-    to prevent hash collision."""
+    to prevent hash collision.
+
+    Use `delta_robust2` for pandas v0.21 onward."""
     df1 = df1.copy()
     df2 = df2.copy()
     if isinstance(index_cols, pd.Index):
@@ -59,6 +67,34 @@ def delta_robust(df1, df2, index_cols, **kwargs):
     for i, col in enumerate(index_cols):
         df1.loc[:, col] = df1[col].astype('category', categories=cats[i]).cat.codes
         df2.loc[:, col] = df2[col].astype('category', categories=cats[i]).cat.codes
+    f, a, c, d = delta(df1, df2, index_cols, **kwargs)
+    for i, col in enumerate(index_cols):
+        f[col] = pd.Categorical.from_codes(f[col], cats[i]).astype(types[i])
+        a[col] = pd.Categorical.from_codes(a[col], cats[i]).astype(types[i])
+        c[col] = pd.Categorical.from_codes(c[col], cats[i]).astype(types[i])
+        d[col] = pd.Categorical.from_codes(d[col], cats[i]).astype(types[i])
+    return f, a, c, d
+
+
+def delta_robust2(df1, df2, index_cols, **kwargs):
+    """Compute delta between two files while encoding index_cols
+    to prevent hash collision.
+
+    Use `delta_robust` before pandas v0.21."""
+    df1 = df1.copy()
+    df2 = df2.copy()
+    if isinstance(index_cols, pd.Index):
+        index_cols = list(index_cols)
+    kwargs.pop('reset_indexes', None)
+    cats = [pd.Categorical(np.union1d(*[df[col].values for df in [df1, df2]]))
+              .categories
+            for col in index_cols]
+    types = [df1[col].dtype for col in index_cols]
+
+    for i, col in enumerate(index_cols):
+        cat_dtype = CategoricalDtype(categories=cats[i])
+        df1.loc[:, col] = df1[col].astype(cat_dtype).cat.codes
+        df2.loc[:, col] = df2[col].astype(cat_dtype).cat.codes
     f, a, c, d = delta(df1, df2, index_cols, **kwargs)
     for i, col in enumerate(index_cols):
         f[col] = pd.Categorical.from_codes(f[col], cats[i]).astype(types[i])
